a24377fde1961996db10b2a4219f4f5c0a947bc2
Maximilien Garenne <maximilien.garenne@gmail.com>
2025-10-26 10:57:21 +0100

    remove comment

--- cmd/gh/label/sync.go
+++ cmd/gh/label/sync.go
@@ -17,24 +17,32 @@ var LabelSyncCommand = &cobra.Command{
 	Short: "Sync GitHub labels from a configuration file",
 	Long:  `Sync GitHub labels from a configuration file.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := git.VerifyCurrentDirectoryIsGitRepo(); err != nil {
-			os.Stderr.WriteString(err.Error() + "\n")
-			os.Exit(1)
-		}
-		if err := gh.VerifyGhAuthenticated(); err != nil {
-			os.Stderr.WriteString(err.Error() + "\n")
-			os.Exit(1)
-		}
-		if err := gh.VerifyRemote(); err != nil {
-			os.Stderr.WriteString(err.Error() + "\n")
-			os.Exit(1)
-		}
-
-		// if err := verifyGhCLIInstalled(); err != nil {
-		// 	os.Stderr.WriteString("GitHub CLI (gh) is not installed or not found in PATH\n")
-		// 	os.Exit(1)
-		// }
-		// Placeholder for the actual label sync logic
+		execute()
 		os.Stdout.WriteString("Label sync command executed\n")
 	},
+}
+
+func execute() {
+
+	if err := git.VerifyCurrentDirectoryIsGitRepo(); err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+	if err := gh.VerifyGhAuthenticated(); err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+	if err := gh.VerifyRemote(); err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	if path, err := git.GetRepoRootPath(); err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	} else {
+		os.Stdout.WriteString("here is the repo/worktree path\n\n")
+		os.Stdout.WriteString(path)
+		os.Stdout.WriteString("\n\n")
+	}
 }
